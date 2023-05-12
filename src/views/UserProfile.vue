e005e014b07aaf4711558f1cd66261d8cdd9b0b8
Emily Shea <c.emilyshea@gmail.com>
2023-05-12 23:06:28 +0100

    remove audio element

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -92,7 +92,6 @@
               <h5 v-if="word.CharacterSet === 'simplified'" class="card-text">{{ word.word.simplified }}</h5>
               <h5 v-if="word.CharacterSet === 'traditional'" class="card-text">{{ word.word.traditional }}</h5>
               <p class="card-text">{{ word.word.pinyin }} <span @click="playAudio(word.word.audio_file_key)" class="oi oi-volume-high audio-icon"></span></p>
-              <audio id="audio" :src="word.word.audio_file_key">"Sorry, your browser does not support audio files."</audio>
               <p class="card-text truncate">{{ word.word.definition }}</p>
               <ul class="list-group list-group-flush">
                 <li class="list-group-item text-muted">HSK Level {{ word.word.hsk_level }}, {{ word.CharacterSet }}</li>
